a905ba6e121654beca53230cc9bfdab37f68c4ed
Shuai Guan <shuaiguan@gmail.com>
2018-06-26 00:03:59 -0400

    Initial commit

---
+++ Data Processing.py
@@ -0,0 +1,31 @@
+#data preprocessing
+#importing library
+
+import numpy as np
+import panda as pd
+import matplotlib.pyplot as plt
+
+#import the dataset
+#you have to first set upwork directory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
