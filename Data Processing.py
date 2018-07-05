844ece3e5bbaf5281642c9bd327811e8ca9fcacd
Shuai Guan <shuaiguan@gmail.com>
2018-07-05 14:34:20 -0400

    finish up the final template

--- Data Processing.py
+++ Data Processing.py
@@ -47,7 +47,7 @@ y = LabelEncoder_y.fit_transform(y)
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-#feature scaling
+#feature scaling, to make all the variables within the same range
 #this part MOST OF THE TIME is included in the ML library
 from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
