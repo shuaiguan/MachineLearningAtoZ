17d3a8bcfd1484027db6fa3e52081ff200b74178
Shuai Guan <shuaiguan@gmail.com>
2018-07-28 18:29:34 -0400

    newest version

--- regression_template.py
+++ regression_template.py
@@ -21,47 +21,25 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 we don't need feature scaling because polynomial 
 '''
 
-#first build linear regression to see the result to compare with polynomial
-from sklearn.linear_model import LinearRegression
-linear_regressor = LinearRegression()
-linear_regressor.fit(x, y)
+#fitting regression model to the dataset
+#create the regressor
 
-#fitting polynomial regression to the dataset
-from sklearn.preprocessing import PolynomialFeatures
-#degree means the power of the variable
-poly_regressor = PolynomialFeatures(degree = 4)
-#need to fit and then transform because it will transform the x
-x_poly = poly_regressor.fit_transform(x)
-linear_regressor_2 = LinearRegression()
-linear_regressor_2.fit(x_poly, y)
+#predicting a new result with polynomial regression
+y_pred = regressor.predict(6.5)
 
-#visualizing the linear regression results
-plt.scatter(x, y, color='red')
-plt.plot(x, linear_regressor.predict(x), color='blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
-plt.show()
 
 #visualizing the polynomial regression results
 #x_grid will contain all the levels and the incremental numbers
+#this step to add more resolution for the visualization of predictions
 x_grid = np.arange(min(x), max(x), 0.1)
 #create a matrix, len(x_grid) is the rows, and 1 is the columns
 x_grid = x_grid.reshape(len(x_grid), 1)
 plt.scatter(x, y, color='red')
-plt.plot(x_grid, linear_regressor_2.predict(poly_regressor.fit_transform(x_grid)), color='blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
+plt.plot(x_grid, regressor.predict(x_grid), color='blue')
+plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
-
-#predicting a new result with Linear Regression
-linear_regressor.predict(6.5)
-
-#predicting a new result with polynomial regression
-linear_regressor_2.predict(poly_regressor.fit_transform(6.5))
-
-
 
 
 
